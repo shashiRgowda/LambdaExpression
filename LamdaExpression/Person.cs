d27bc9cd3cdbf528b7e8aa9fe5a3dfef2f0bbffb
shashiRgowda <shashikumartr64@gmail.com>
2021-05-21 20:01:15 +0530

    Added:LamdaExpression

---
+++ LamdaExpression/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamdaExpression
+{
+    class Person
+    {
+
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+    }
+
+}
